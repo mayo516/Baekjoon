413a595dfbce9a729c9c3902c1c588b0ee9f14eb
eunha <70703716+mayo516@users.noreply.github.com>
2024-08-11 20:59:38 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+for _ in range(T):
+    stk = []
+    isVPS = True
+    for ch in input():
+        if ch == '(':
+            stk.append(ch)
+        else:
+            if stk:
+                stk.pop()
+            else:
+                isVPS = False
+                break
+    if stk:
+        isVPS = False
+    print('YES' if isVPS else 'NO')
+            
